ac26a4c380b84069df20b3b5c99953f36d968e7d
hssen139 <hssenshamas@gmail.com>
2022-06-17 11:00:43 +0300

    Building a dynamic array from scratch

---
+++ src/Arrays.java
@@ -0,0 +1,53 @@
+
+/*
+                      Building a dynamic array where you can add , remove , search elements with no boundaries.
+
+                                     Created by hussein shamas.
+ */
+
+public class Arrays {
+
+    private int[] array;
+    private int count;
+
+    public Arrays(int length) {
+
+        array = new int[length];
+
+    }
+
+    public void insert(int item) {
+        if (array.length == count) {
+            int[] newArray = new int[count * 2];
+            for (int i = 0; i < count; i++)
+                newArray[i] = array[i];
+
+            array = newArray;
+        }
+        array[count] = item;
+        count++;
+    }
+
+    public void removeAt(int index) {
+        if (index < 0 || index >= count) throw new IllegalArgumentException();
+        for (int i = index; i < count; i++)
+            array[i] = array[i + 1];
+        count--;
+
+    }
+
+    public int indexOf(int item) {
+        for (int i = 0; i < count; i++)
+            if (array[i] == item) {
+                return i;
+            }
+
+        return -1;
+    }
+
+    public void print() {
+        for (int i = 0; i < count; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,24 @@
+
+/*
+               Give it a try.
+
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+        // debugging
+
+      /*
+
+       Arrays arrays = new Arrays(3);
+       arrays.insert(29);
+       arrays.insert(33);
+       arrays.insert(4545);
+       arrays.insert(44);
+       System.out.println(arrays.indexOf(44));
+
+
+       */
+    }
+}
